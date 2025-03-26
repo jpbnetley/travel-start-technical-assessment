9c4476cf65cca1e24a941d63e6b82bfcf958a7a3
Jonathan Netley <jpbnetley@gmail.com>
2025-03-26 14:47:29 +0200

    build: rm qs dep

--- src/utils/handlers/query-string-builder.ts
+++ src/utils/handlers/query-string-builder.ts
@@ -5,14 +5,8 @@ import qs from "qs"
  * @param urlObject that is used to build the query string from
  * @returns query string
  */
-export const queryStringBuilder = <QueryObject extends Record<string, any>>(urlObject: QueryObject) => {
-  const paramsString = qs.stringify(
-    urlObject,
-  {
-    skipNulls: true,
-    // https://github.com/ljharb/qs?tab=readme-ov-file#rfc-3986-and-rfc-1738-space-encoding
-    format : 'RFC1738' 
-  })
-
+export const queryStringBuilder = <QueryObject extends Record<string, string>>(urlObject: QueryObject) => {
+  const params = new URLSearchParams(urlObject)
+  const paramsString =  params.toString()
   return paramsString && `?${paramsString}`
 }
